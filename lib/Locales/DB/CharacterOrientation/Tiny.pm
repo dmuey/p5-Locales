2dbf2e22af42a49bd7b20dd46ceeda5b63012ac0
Daniel Muey <simplemood.com@gmail.com>
2013-09-20 17:55:17 -0500

    Initial repo at v0.27

---
+++ lib/Locales/DB/CharacterOrientation/Tiny.pm
@@ -0,0 +1,32 @@
+package Locales::DB::CharacterOrientation::Tiny;
+
+# Auto generated from CLDR
+
+$Locales::DB::CharacterOrientation::Tiny::VERSION = '0.09';
+
+$Locales::DB::CharacterOrientation::Tiny::cldr_version = '2.0';
+
+my %rtl = (
+    'ur' => '',
+    'ku' => '',
+    'he' => '',
+    'fa' => '',
+    'ps' => '',
+    'ar' => '',
+);
+
+sub get_orientation {
+    if ( exists $rtl{ $_[0] } ) {
+        return 'right-to-left';
+    }
+    else {
+        require Locales;
+        my ($l) = Locales::split_tag( $_[0] );
+        if ( $l ne $_[0] ) {
+            return 'right-to-left' if exists $rtl{$l};
+        }
+        return 'left-to-right';
+    }
+}
+
+1;
---
+++ lib/Locales/DB/LocaleDisplayPattern/Tiny.pm
@@ -0,0 +1,30 @@
+package Locales::DB::LocaleDisplayPattern::Tiny;
+
+# Auto generated from CLDR
+
+$Locales::DB::LocaleDisplayPattern::Tiny::VERSION = '0.09';
+
+$Locales::DB::LocaleDisplayPattern::Tiny::cldr_version = '2.0';
+
+my %locale_display_lookup = (
+    'ksh' => '{0} en {1}',
+    'ja'  => '{0}({1})',
+    'zh'  => '{0}（{1}）',
+    'ko'  => '{0}({1})',
+);
+
+sub get_locale_display_pattern {
+    if ( exists $locale_display_lookup{ $_[0] } ) {
+        return $locale_display_lookup{ $_[0] };
+    }
+    else {
+        require Locales;
+        my ($l) = Locales::split_tag( $_[0] );
+        if ( $l ne $_[0] ) {
+            return $locale_display_lookup{$l} if exists $locale_display_lookup{$l};
+        }
+        return "\{0\}\ \(\{1\}\)";
+    }
+}
+
+1;
